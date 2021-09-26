f5109094f5668a3f575fc1a6c9011a67ebe4220b
fpemud <fpemud@163.com>
2021-09-26 17:13:51 +0800

    fix

---
+++ python3/strict_hdds/layout_efi_btrfs.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2020-2021 Fpemud <fpemud@sina.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+import os
+import re
+from . import util
+from . import StorageLayout
+
+
+class StorageLayoutEfiBtrfs(StorageLayout):
+    """Layout:
+           /dev/sda                 GPT, EFI-GRUB
+               /dev/sda1            ESP partition
+               /dev/sda2            btrfs device
+           /dev/sdb                 GPT
+               /dev/sdb1            reserved ESP partition
+               /dev/sdb2            btrfs device
+           /dev/sda1:/dev/sda2      root device, btrfs
+       Description:
+           1. /dev/sda1 and /dev/sdb1 must has the same size
+           2. /dev/sda1 and /dev/sda2 is order-sensitive, no extra partition is allowed
+           3. /dev/sdb1 and /dev/sdb2 is order-sensitive, no extra partition is allowed
+           4. use optional swap file /var/swap/swap, at this time /var/swap is a standalone sub-volume
+           5. extra harddisk is allowed to exist
+    """
+
+    pass
+
+    name = "efi-btrfs"
