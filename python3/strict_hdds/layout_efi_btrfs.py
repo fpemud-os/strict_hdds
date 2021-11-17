babb24b5f907d79ede785ba6bcf6c249cb789a67
fpemud <fpemud@sina.com>
2021-11-17 20:03:29 +0800

    fix

---
+++ python3/strict_hdds/layout_efi_btrfs.py
@@ -0,0 +1,185 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2020-2021 Fpemud <fpemud@sina.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+
+from .util import Util, PartiUtil, GptUtil, EfiMultiDisk, BtrfsUtil
+from .handy import MountEfi, HandyUtil
+from . import errors
+from . import StorageLayout
+
+
+class StorageLayoutImpl(StorageLayout):
+    """Layout:
+           /dev/sda                 GPT
+               /dev/sda1            ESP partition
+               /dev/sda2            btrfs device
+           /dev/sdb                 GPT
+               /dev/sdb1            reserved ESP partition
+               /dev/sdb2            btrfs device
+           /dev/sda1:/dev/sda2      root device, btrfs
+       Description:
+           1. /dev/sda1 and /dev/sdb1 must has the same size
+           2. /dev/sda1 and /dev/sda2 is order-sensitive, no extra partition is allowed
+           3. /dev/sdb1 and /dev/sdb2 is order-sensitive, no extra partition is allowed
+           4. use optional swap file /var/swap/swap.dat, at this time /var/swap is a standalone sub-volume
+           5. extra harddisk is allowed to exist
+    """
+
+    def __init__(self):
+        self._md = None              # MultiDisk
+        self._mnt = None             # MountEfi
+
+    @property
+    def boot_mode(self):
+        return StorageLayout.BOOT_MODE_EFI
+
+    @property
+    def dev_rootfs(self):
+        return _getDevRoot(self._md)
+
+    @property
+    @EfiMultiDisk.proxy
+    def dev_boot(self):
+        pass
+
+    @property
+    def dev_swap(self):
+        assert False
+
+    @EfiMultiDisk.proxy
+    def boot_disk(self):
+        pass
+
+    def umount_and_dispose(self):
+        if True:
+            self._mnt.umount()
+            del self._mnt
+        del self._md
+
+    @MountEfi.proxy
+    def remount_rootfs(self, mount_options):
+        pass
+
+    @MountEfi.proxy
+    def get_bootdir_rw_controller(self):
+        pass
+
+    def check(self):
+        pass
+
+    @EfiMultiDisk.proxy
+    def get_esp(self):
+        pass
+
+    @EfiMultiDisk.proxy
+    def get_pending_esp_list(self):
+        pass
+
+    @EfiMultiDisk.proxy
+    def sync_esp(self, dst):
+        pass
+
+    @EfiMultiDisk.proxy
+    def get_disk_list(self):
+        pass
+
+    @EfiMultiDisk.proxy
+    def get_disk_esp_partition(self, disk):
+        pass
+
+    @EfiMultiDisk.proxy
+    def get_disk_data_partition(self, disk):
+        pass
+
+    def add_disk(self, disk):
+        assert disk is not None
+
+        if disk not in Util.getDevPathListForFixedDisk():
+            raise errors.StorageLayoutAddDiskError(disk, errors.NOT_DISK)
+
+        lastBootDisk = self._md.boot_disk
+
+        # add
+        self._md.add_disk(disk)
+
+        # hdd partition 2: make it as backing device and add it to btrfs filesystem
+        Util.cmdCall("/sbin/btrfs", "device", "add", self._md.get_disk_data_partition(disk), "/")
+
+        return lastBootDisk != self._md.boot_disk     # boot disk may change
+
+    def remove_disk(self, disk):
+        assert disk is not None
+        assert disk in self._md.get_disk_list()
+
+        if len(self._md.get_disk_list()) <= 1:
+            raise errors.StorageLayoutRemoveDiskError(errors.CAN_NOT_REMOVE_LAST_HDD)
+
+        lastBootHdd = self._md.boot_disk
+
+        # hdd partition 2: remove from btrfs and bcache
+        Util.cmdCall("/sbin/btrfs", "device", "delete", self._md.get_disk_data_partition(disk), "/")
+
+        # remove
+        self._md.remove_disk(disk)
+
+        return lastBootHdd != self._md.boot_disk     # boot disk may change
+
+
+def parse(boot_dev, root_dev):
+    if not GptUtil.isEspPartition(boot_dev):
+        raise errors.StorageLayoutParseError(StorageLayoutImpl.name, errors.BOOT_DEV_IS_NOT_ESP)
+    if Util.getBlkDevFsType(root_dev) != Util.fsTypeBtrfs:
+        raise errors.StorageLayoutParseError(StorageLayoutImpl.name, errors.ROOT_PARTITION_FS_SHOULD_BE(Util.fsTypeBtrfs))
+
+    # get disk list and check
+    diskList = BtrfsUtil.getSlaveDevPathList(root_dev)
+    bootHdd = PartiUtil.partiToDisk(boot_dev)
+    if bootHdd not in diskList:
+        raise errors.StorageLayoutParseError(StorageLayoutImpl.name, errors.BOOT_DISK_MUST_IN_SLAVE_DISK_LIST)
+
+    # return
+    ret = StorageLayoutImpl()
+    ret._md = EfiMultiDisk(diskList=diskList, bootHdd=bootHdd)
+    ret._mnt = MountEfi("/")
+    return ret
+
+
+def create_and_mount(disk_list=None):
+    if disk_list is None:
+        disk_list = Util.getDevPathListForFixedDisk()
+        if len(disk_list) == 0:
+            raise errors.StorageLayoutCreateError(errors.NO_DISK_WHEN_CREATE)
+    else:
+        assert len(disk_list) > 0
+
+    ret = StorageLayoutImpl()
+
+    ret._md = EfiMultiDisk()
+    for devpath in disk_list:
+        ret._md.add_disk(devpath)
+
+    return ret
+
+
+
+def _getDevRoot(md):
+    return md.get_disk_list()[0]
